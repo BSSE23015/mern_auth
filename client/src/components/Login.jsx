5fc7cc9ded984c27fbf61a0988bccbc71ddceba4
Hassan Abid 15 <bsse23015@itu.edu.pk>
2026-03-21 20:16:57 +0500

    Project Completed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,7 +6,68 @@ import { Context } from "../main";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <></>;
+  const {  setIsAuthenticated, user, setUser } =
+    useContext(Context);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleLogin = async (data) => {
+    await axios
+      .post("http://localhost:4000/api/v1/login", data, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        toast.success(res.data.message);
+        setIsAuthenticated(true);
+        setUser(res.data.user);
+        navigate("/");
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+  return (
+    <>
+      <form
+        className="auth-form"
+        onSubmit={handleSubmit((data) => handleLogin(data))}
+      >
+        <h2>Login</h2>
+        <h3>Enter Email</h3>
+        <input
+          placeholder="Email"
+          type="email"
+          required
+          {...register("email", {
+            required: "Email is required!",
+          })}
+        />
+        <h3>Enter Password</h3>
+        <input
+          placeholder="Password"
+          type="password"
+          required
+          {...register("password", {
+            required: "Password is required!",
+          })}
+        />
+        {errors.password && (
+          <p style={{ color: "red" }}>{errors.password.message}</p>
+        )}
+        <p className="forgot-password">
+          Forgot Password?
+          <Link to={"/password/forgot"}>Click Here</Link>
+        </p>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../styles/Auth.css";
-
+import { Context } from "../main";
+import { Navigate } from "react-router-dom";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import { useState } from "react";
 const Auth = () => {
-  return <></>;
+  const { isAuthenticated } = useContext(Context);
+  const [isLogin, setIsLogin] = useState(true);
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <>
+      <div className="auth-page">
+        <div className="auth-container">
+          <div className="auth-toggle">
+            <button
+              className={`toggle-btn ${isLogin ? "active" : ""}`}
+              onClick={() => setIsLogin(true)}
+            >
+              Login
+            </button>
+            <button
+              className={`toggle-btn ${!isLogin ? "active" : ""}`}
+              onClick={() => setIsLogin(false)}
+            >
+              Register
+            </button>
+          </div>
+          {isLogin ? <Login /> : <Register />}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Auth;
